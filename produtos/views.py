af6f0c6f4b005b42d0eeca86539b3f23b37a9a86
Eduardo <ceduardorm2000@gmail.com>
2023-02-08 21:31:06 -0300

    adicionando model de produtos

--- produtos/views.py
+++ produtos/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 
 def listagem_de_produtos(request):
-    contex = {
-        'produtos':[
-        {'nome': 'uva','preco':1}, 
-        {'nome': 'melancia','preco':2},
-        {'nome': 'banana','preco': 5}
 
+    
+        produtos_de_vendedores = [{
+            'vendedor': {'nome': 'john Doe'},
+            'produtos':[
+                {'nome': 'uva','preco':1}, 
+                {'nome': 'melancia','preco':2},
+                {'nome': 'banana','preco': 5}
         ]
-    }
-    return render(request, 'templates/listagem_de_produtos.html.', contex)
+        }]
+        context = {'produtos_de_vendedores': produtos_de_vendedores}
+    
+        return render(request, 'templates/listagem_de_produtos.html.', context)
 
 # Create your views here.
