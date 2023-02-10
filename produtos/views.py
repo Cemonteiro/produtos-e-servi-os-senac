46856ebeb638d7e80fbbba246e5ab771cede8d5e
Eduardo <ceduardorm2000@gmail.com>
2023-02-10 19:48:53 -0300

    adiciona fuçao cadastro produto

--- produtos/views.py
+++ produtos/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from produtos.models import Produto
 from produtos.forms import ProdutoModelForm
-
+from produtos.forms import HttpResponseRedirect
 def listagem_de_produtos(request):
         produto = Produto.objects.all()
         produtos_dos_vendedores = [{
@@ -23,6 +23,19 @@ def cadastrar_produto(request):
         context = {
                 'form' : form
         }
+        if request.method == 'GET':
+                print('É um get')
+        if request.method == 'POST':
+                form = ProdutoModelForm(request.POST)
+                if form.is_valid():
+                        form.save()
+                        return HttpResponseRedirect('/produtos/')
+                
+        form = ProdutoModelForm()
+        context = {
+                'form': form
+        }
+
         return render(request, 'templates/cadastrar_produto.html', context)
 
 
